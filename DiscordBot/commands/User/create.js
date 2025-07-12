23d6f5ea40e1d035c7b24eca26ec767ced4148a0
Alby <Albykick@gmail.com>
2025-07-12 16:37:30 +0200

    updated create so when u register only you can see it and its cleaner and like yeah

--- DiscordBot/commands/User/create.js
+++ DiscordBot/commands/User/create.js
@@ -1,21 +1,15 @@
 const { MessageEmbed } = require("discord.js");
-const User = require("../../../model/user.js")
+const User = require("../../../model/user.js");
 const functions = require("../../../structs/functions.js");
 
 module.exports = {
     commandInfo: {
         name: "create",
-        description: "Creates an account on Reload Backend.",
+        description: "Create an account",
         options: [
             {
                 name: "email",
                 description: "Your email.",
-                required: true,
-                type: 3
-            },
-            {
-                name: "username",
-                description: "Your username.",
                 required: true,
                 type: 3
             },
@@ -34,14 +28,11 @@ module.exports = {
 
         const discordId = interaction.user.id;
         const email = options.get("email").value;
-        const username = options.get("username").value;
         const password = options.get("password").value;
+        const username = interaction.user.username;
 
-        const plainEmail = options.get('email').value;
-        const plainUsername = options.get('username').value;
-
-        const existingEmail = await User.findOne({ email: plainEmail });
-        const existingUser = await User.findOne({ username: plainUsername });
+        const existingEmail = await User.findOne({ email: email });
+        const existingUser = await User.findOne({ username: username });
 
         const emailFilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         if (!emailFilter.test(email)) {
@@ -51,13 +42,7 @@ module.exports = {
             return interaction.editReply({ content: "Email is already in use, please choose another one.", ephemeral: true });
         }
         if (existingUser) {
-            return interaction.editReply({ content: "Username already exists. Please choose a different one.", ephemeral: true });
-        }
-        if (username.length >= 25) {
-            return interaction.editReply({ content: "Your username must be less than 25 characters long.", ephemeral: true });
-        }
-        if (username.length < 3) {
-            return interaction.editReply({ content: "Your username must be at least 3 characters long.", ephemeral: true });
+            return interaction.editReply({ content: "You already have an account.", ephemeral: true });
         }
         if (password.length >= 128) {
             return interaction.editReply({ content: "Your password must be less than 128 characters long.", ephemeral: true });
@@ -68,28 +53,11 @@ module.exports = {
 
         await functions.registerUser(discordId, username, email, password).then(resp => {
             let embed = new MessageEmbed()
-            .setColor(resp.status >= 400 ? "#ff0000" : "#56ff00")
-            .setThumbnail(interaction.user.avatarURL({ format: 'png', dynamic: true, size: 256 }))
-            .addFields({
-                name: "Message",
-                value: "Successfully created an account.",
-            }, {
-                name: "Username",
-                value: username,
-            }, {
-                name: "Discord Tag",
-                value: interaction.user.tag,
-            })
-            .setTimestamp()
-            .setFooter({
-                text: "Reload Backend",
-                iconURL: "https://i.imgur.com/2RImwlb.png"
-            })
+                .setColor("#A020F0")
+                .setDescription(`Welcome to Reload Baceknd , **${username}**, have a great time!`) 
+                .setImage("https://i.imgur.com/dSGYw51.png"); // image
 
-            if (resp.status >= 400) return interaction.editReply({ embeds: [embed], ephemeral: true });
-
-            (interaction.channel ? interaction.channel : interaction.user).send({ embeds: [embed] });
-            interaction.editReply({ content: "You successfully created an account!", ephemeral: true });
+            interaction.editReply({ embeds: [embed], ephemeral: true });
         });
     }
 }
