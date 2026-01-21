613f246aa01181cac0f06ae72b42555aecdf23b6
Alby <Albykick@gmail.com>
2026-01-21 22:28:18 +0100

    Long time no see (fixed settings savings on later szns)

--- routes/cloudstorage.js
+++ routes/cloudstorage.js
@@ -7,7 +7,7 @@ const path = require("path");
 const { verifyToken, verifyClient } = require("../tokenManager/tokenVerify.js");
 const functions = require("../structs/functions.js");
 
-let seasons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let seasons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
 
 app.get("/fortnite/api/cloudstorage/system", (req, res) => {
     const dir = path.join(__dirname, "..", "CloudStorage");
